536fa4da1a7b6c5f622b16c019d72ff7e19ad76e
kerry <kerrywang369@gmail.com>
2022-01-07 21:17:16 -0800

    added button support

---
+++ discord-bot/commands/ping copy.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('jesus')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY'),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	},
+};
